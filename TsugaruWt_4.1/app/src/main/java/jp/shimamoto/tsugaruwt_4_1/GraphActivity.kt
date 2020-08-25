795d02f2d7bbfbe4aeb4e27e59f201631fc6a003
shimamoto-g1 <caldina222-123@yahoo.co.jp>
2020-08-25 11:03:09 +0900

    ツガルの重さ 8/25 『Kotlin MPAndroidChartライブラリを使い移動平均線を描画してみる。』 をグラフ描画に移植。表示まで確認OK

--- TsugaruWt_4.1/app/src/main/java/jp/shimamoto/tsugaruwt_4_1/GraphActivity.kt
+++ TsugaruWt_4.1/app/src/main/java/jp/shimamoto/tsugaruwt_4_1/GraphActivity.kt
@@ -3,18 +3,107 @@ package jp.shimamoto.tsugaruwt_4_1
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import io.realm.Realm
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import kotlinx.android.synthetic.main.activity_graph.*
-import java.text.SimpleDateFormat
-import java.util.*
 
-open class GraphActivity : AppCompatActivity() {
+open class GraphActivity : AppCompatActivity() , OnChartValueSelectedListener {
+
+    //Typefaceの設定、後ほど使用します。
+    private var mTypeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
+
+    private val chartDataCount = 20
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graph)
+
+        setupLineChart()
+        lineChart.data = lineDataWithCount(chartDataCount, 100f)
+        
+        }
+
+    private fun setupLineChart(){
+        lineChart.apply {
+            setOnChartValueSelectedListener(this@GraphActivity)
+            description.isEnabled = false
+            setTouchEnabled(true)
+            isDragEnabled = true
+            isScaleXEnabled = true
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+
+            //データラベルの表示
+            legend.apply {
+                form = Legend.LegendForm.LINE
+                typeface = mTypeface
+                textSize = 11f
+                textColor = Color.BLACK
+                verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+                horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+                orientation = Legend.LegendOrientation.HORIZONTAL
+                setDrawInside(false)
+            }
+
+            //y軸右側の表示
+            axisRight.isEnabled = false
+
+            //X軸表示
+            xAxis.apply {
+                typeface = mTypeface
+                setDrawLabels(false)
+                setDrawGridLines(true)
+            }
+
+            //y軸左側の表示
+            axisLeft.apply {
+                typeface = mTypeface
+                textColor = Color.BLACK
+                setDrawGridLines(true)
+            }
+        }
+    }
+
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        Log.i("Entry selected", e.toString())
+    }
+
+    override fun onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.")
+    }
+
+    //    LineChart用のデータ作成
+    private fun lineDataWithCount(count: Int, range: Float):LineData {
+
+        val values = mutableListOf<Entry>()
+
+        for (i in 0 until count) {
+            val value = (Math.random() * (range)).toFloat()
+            values.add(Entry(i.toFloat(), value))
+        }
+        // create a dataset and give it a type
+        val yVals = LineDataSet(values, "SampleLineData").apply {
+            axisDependency =  YAxis.AxisDependency.LEFT
+            color = Color.BLACK
+            highLightColor = Color.YELLOW
+            setDrawCircles(false)
+            setDrawCircleHole(false)
+            setDrawValues(false)
+            lineWidth = 2f
+        }
+        val data = LineData(yVals)
+        data.setValueTextColor(Color.BLACK)
+        data.setValueTextSize(9f)
+        return data
+    }
+    /*
 
     private lateinit var realm: Realm   //// 8/21
 
@@ -191,4 +280,6 @@ open class GraphActivity : AppCompatActivity() {
         super.onDestroy()    //// 8/21
         realm.close()    //// 8/21
     }
+
+     */
 }
